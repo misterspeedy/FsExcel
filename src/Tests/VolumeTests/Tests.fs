31f0e15cd30c0ba65d00d0a1a5bd211f3a0840a3
Kit Eason <kit.eason@perpetuum.com>
2022-03-08 08:48:20 +0000

    Add a very basic volume test.

---
+++ src/Tests/VolumeTests/Tests.fs
@@ -0,0 +1,21 @@
+module Tests
+
+open System
+open System.IO
+open Xunit
+open FsExcel
+
+let tempPath = "/temp"
+
+[<Fact>]
+let ``Volume test 1`` () =
+    let rows = 1000
+    let cols = 1000
+    [
+        for row in 1..rows do
+            for col in 1..cols do
+                Cell [ Integer (row*col)]
+            Go NewRow
+    ]
+    |> FsExcel.Render.AsFile(Path.Combine(tempPath, "VolumeTest1.xlsx")) 
+    Assert.True(true)
