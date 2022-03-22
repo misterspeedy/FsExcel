d8c4350054a8f6288c302e22a8aee461b9781342
John Cavnar-Johnson <john.cavnar-johnson@improving.com>
2022-03-22 17:37:00 -0500

    InsertRowsAbove WIP

--- src/FsExcel/FsExcel.fs
+++ src/FsExcel/FsExcel.fs
@@ -86,6 +86,7 @@ type Item =
     | Worksheet of string
     | AutoFit of AutoFit
     | Workbook of XLWorkbook
+    | InsertRowsAbove of int
 
 module Render = 
 
@@ -155,6 +156,8 @@ module Render =
                 then currentWorksheet <- wb.Worksheet(name) |> Some
                 else currentWorksheet <- wb.Worksheets.Add(name) |> Some
                 reset()
+            | InsertRowsAbove rs -> 
+                currentWorksheet.Value.Row(r).InsertRowsAbove(rs)   |> ignore       
             | Go p ->
                 go p
             | Cell props ->
